64ab71d98c265a4669207159acfb068dcf0055a3
Bruno Sabino <bruno.p.sabino@gmail.com>
2021-02-17 22:06:49 -0300

    Using Custom Components

--- my-first-app/src/app/server/server.component.ts
+++ my-first-app/src/app/server/server.component.ts
@@ -1,10 +1,8 @@
 import { Component } from "@angular/core";
 
-import { Component } from '@angular/core';
-
 @Component({
     selector: 'app-server',
-    template: './server.component.html'
+    templateUrl: './server.component.html'
 })
 export class ServerComponent {
 
